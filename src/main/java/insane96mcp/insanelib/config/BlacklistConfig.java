8e8c455a79d6587465f6fd2e2bd6b12e5be8cf60
Alberto Del Villano <delvillano.alberto@gmail.com>
2021-08-28 10:55:34 +0200

    Added BlacklistConfig

---
+++ src/main/java/insane96mcp/insanelib/config/BlacklistConfig.java
@@ -0,0 +1,29 @@
+package insane96mcp.insanelib.config;
+
+import insane96mcp.insanelib.setup.Config;
+import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.util.List;
+
+public class BlacklistConfig {
+	public ForgeConfigSpec.ConfigValue<List<? extends String>> listConfig;
+	public ForgeConfigSpec.ConfigValue<Boolean> listAsWhitelistConfig;
+
+	/**
+	 * Creates a new Black/Whitelist config, containing the list, and if the list should be treated as a whitelist
+	 * @param builder Config Builder
+	 * @param listName Name of the list
+	 * @param description Description for the list
+	 * @param defaultList The default list
+	 * @param isDefaultWhitelist If true, the list will default to whitelist instead of blacklist
+	 */
+	public BlacklistConfig(ForgeConfigSpec.Builder builder, String listName, String description, List<String> defaultList, boolean isDefaultWhitelist) {
+		builder.comment(description).push(listName);
+		listConfig = Config.builder
+				.defineList("Blacklist", defaultList, o -> o instanceof String);
+		listAsWhitelistConfig = Config.builder
+				.comment("If true the list will be treated as a whitelist instead of blacklist")
+				.define("List as Whitelist", isDefaultWhitelist);
+		builder.pop();
+	}
+}
