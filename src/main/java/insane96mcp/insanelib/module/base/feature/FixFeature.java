a0e05ea8ddbbb871e0e5ffb060de891bc58139f0
Alberto Del Villano <delvillano.alberto@gmail.com>
2021-09-26 21:53:10 +0200

    Added Fixes Feature. Closes #5. Closes #7

--- src/main/java/insane96mcp/insanelib/module/base/feature/FixFeature.java
+++ src/main/java/insane96mcp/insanelib/module/base/feature/FixFeature.java
@@ -4,10 +4,17 @@ import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.Label;
 import insane96mcp.insanelib.base.Module;
 import insane96mcp.insanelib.setup.Config;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.PrioritizedGoal;
 import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -35,7 +42,7 @@ public class FixFeature extends Feature {
 				.comment("Removes the random bonus health given to Leader Zombies. In vanilla it's useless since doesn't work. https://minecraft.fandom.com/wiki/Attribute#Vanilla_modifiers")
 				.define("Remove Zombies Bonus Health", this.removeZombiesBonusHealth);
 		this.fixJumpMovementFactorConfig = Config.builder
-				.comment("When affected by slowness the player can still jump really far away. When true that will no longer be the case.")
+				.comment("When affected by slowness the player can still jump really far away. When true, jumps length will be calculated base off player's movement speed.")
 				.define("Fix Jump Movement Factor", this.fixJumpMovementFactor);
 		Config.builder.pop();
 	}
@@ -53,13 +60,18 @@ public class FixFeature extends Feature {
 		if (!this.isEnabled())
 			return;
 
+		fixFollowRange(event.getEntity());
+		removeZombiesBonusHealth(event.getEntity());
+	}
+
+	private void removeZombiesBonusHealth(Entity entity) {
 		if (!this.removeZombiesBonusHealth)
 			return;
 
-		if (!(event.getEntity() instanceof ZombieEntity))
+		if (!(entity instanceof ZombieEntity))
 			return;
 
-		ZombieEntity zombie = (ZombieEntity) event.getEntity();
+		ZombieEntity zombie = (ZombieEntity) entity;
 		if (zombie.getAttribute(Attributes.MAX_HEALTH) == null)
 			return;
 
@@ -67,6 +79,46 @@ public class FixFeature extends Feature {
 		for (AttributeModifier attributeModifier : modifiers)
 			if (attributeModifier.getName().equals("Leader zombie bonus"))
 				zombie.getAttribute(Attributes.MAX_HEALTH).removeModifier(attributeModifier.getID());
+	}
 
+	private void fixFollowRange(Entity entity) {
+		if (!this.fixFollowRange)
+			 return;
+
+		if (!(entity instanceof MobEntity))
+			return;
+
+		MobEntity mobEntity = (MobEntity) entity;
+
+		ModifiableAttributeInstance followRangeAttribute = mobEntity.getAttribute(Attributes.FOLLOW_RANGE);
+		if (followRangeAttribute != null) {
+			for (PrioritizedGoal pGoal : mobEntity.targetSelector.goals) {
+				if (pGoal.getGoal() instanceof NearestAttackableTargetGoal) {
+					NearestAttackableTargetGoal<? extends LivingEntity> nearestAttackableTargetGoal = (NearestAttackableTargetGoal<? extends LivingEntity>) pGoal.getGoal();
+					nearestAttackableTargetGoal.targetEntitySelector.setDistance(mobEntity.getAttributeValue(Attributes.FOLLOW_RANGE));
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public void onUpdate(TickEvent.PlayerTickEvent event) {
+		if (!this.isEnabled())
+			return;
+
+		if (event.phase != TickEvent.Phase.START)
+			return;
+
+		float baseJMF = 0.02f;
+		if (event.player.isSprinting())
+			baseJMF += 0.006f;
+
+		double baseMS = 0.1d;
+		if (event.player.isSprinting())
+			baseMS += 0.03f;
+
+		double playerMS = event.player.getAttributeValue(Attributes.MOVEMENT_SPEED);
+
+		event.player.jumpMovementFactor = (float) ((playerMS * baseJMF) / baseMS);
 	}
 }
