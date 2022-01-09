778e9d8c009485d9ab8a2793723d791f15b200eb
Alberto Del Villano <delvillano.alberto@gmail.com>
2022-01-09 09:19:26 +0100

    Removed LogHelper for other Loggers

--- src/main/java/insane96mcp/insanelib/utils/LogHelper.java
+++ src/main/java/insane96mcp/insanelib/utils/LogHelper.java
@@ -1,7 +1,6 @@
 package insane96mcp.insanelib.utils;
 
 import insane96mcp.insanelib.InsaneLib;
-import org.apache.logging.log4j.Logger;
 
 public class LogHelper {
     public static void error(String s, Object... args) {
@@ -14,17 +13,5 @@ public class LogHelper {
 
     public static void info(String s, Object... args) {
         InsaneLib.LOGGER.info(String.format(s, args));
-    }
-
-    public static void error(Logger logger, String s, Object... args) {
-        logger.error(String.format(s, args));
-    }
-
-    public static void warn(Logger logger, String s, Object... args) {
-        logger.warn(String.format(s, args));
-    }
-
-    public static void info(Logger logger, String s, Object... args) {
-        logger.info(String.format(s, args));
     }
 }
