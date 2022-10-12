c64cbd04f3a52b79dea3cef8a2d07bba1991b7b4
Alberto <delvillano.alberto@gmail.com>
2022-10-12 13:20:24 +0200

    Forgot to re-enable tags

--- src/main/java/insane96mcp/insanelib/module/base/BaseModule.java
+++ src/main/java/insane96mcp/insanelib/module/base/BaseModule.java
@@ -4,20 +4,21 @@ import insane96mcp.insanelib.base.Label;
 import insane96mcp.insanelib.base.Module;
 import insane96mcp.insanelib.module.base.feature.AEC3DFeature;
 import insane96mcp.insanelib.module.base.feature.FixFeature;
+import insane96mcp.insanelib.module.base.feature.TagsFeature;
 import insane96mcp.insanelib.setup.Config;
 
 @Label(name = "Base")
 public class BaseModule extends Module {
 	public AEC3DFeature aec3D;
 	public FixFeature fix;
-	//public TagsFeature tags;
+	public TagsFeature tags;
 
 	public BaseModule() {
 		super(Config.builder, true, false);
 		this.pushConfig();
 		aec3D = new AEC3DFeature(this);
 		fix = new FixFeature(this);
-		//tags = new TagsFeature(this);
+		tags = new TagsFeature(this);
 		this.popConfig();
 	}
 
@@ -26,6 +27,6 @@ public class BaseModule extends Module {
 		super.loadConfig();
 		aec3D.loadConfig();
 		fix.loadConfig();
-		//tags.loadConfig();
+		tags.loadConfig();
 	}
 }
