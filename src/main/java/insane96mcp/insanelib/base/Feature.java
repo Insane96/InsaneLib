54f74e51eb0d0c9ce5989cbde6fdedb996ef83ae
Insane96 <delvillano.alberto@gmail.com>
2024-04-26 09:28:08 +0200

    1.13.3 - Features are no longer enabled on config reload if they can't be disabled

--- src/main/java/insane96mcp/insanelib/base/Feature.java
+++ src/main/java/insane96mcp/insanelib/base/Feature.java
@@ -184,8 +184,6 @@ public class Feature {
     public void readConfig(final ModConfigEvent event) {
         if (canBeDisabled)
             this.enabled = enabledConfig.get();
-        else
-            this.enabled = true;
 
         readConfigOptions();
     }
