ed564b049865de8aad133efde1f8546a22e49f7c
Insane96 <delvillano.alberto@gmail.com>
2025-11-30 16:59:14 +0100

    Uhm

--- src/main/java/insane96mcp/insanelib/mixin/FallingBlockEntityMixin.java
+++ src/main/java/insane96mcp/insanelib/mixin/FallingBlockEntityMixin.java
@@ -206,13 +206,14 @@ public abstract class FallingBlockEntityMixin extends Entity implements BetterFa
         boolean shouldHydrate = isConcretePowder && canBeHydrated;
         boolean isFreeBelow = (FallingBlock.isFree(blockStateBelow) || canBreakBelow) && !shouldHydrate;
 
-        if (isFreeBelow && this.getDeltaMovement().length() == 0) {
-            BlockPos posOn = this.getOnPos();
-            this.move(MoverType.SELF, new Vec3(
-                Vec3.atCenterOf(this.blockPosition()).x - this.position().x,
-                0d,
-                Vec3.atCenterOf(this.blockPosition()).z - this.position().z
-            ));
+        if (isFreeBelow) {
+            if (this.getDeltaMovement().length() == 0) {
+                this.move(MoverType.SELF, new Vec3(
+                        Vec3.atCenterOf(this.blockPosition()).x - this.position().x,
+                        0d,
+                        Vec3.atCenterOf(this.blockPosition()).z - this.position().z
+                ));
+            }
         } else if (canReplaceAtPos || canBreakAtPos) {
             this.insanelib$place(blockstate, block, blockPos, canBreakAtPos);
         } else {
