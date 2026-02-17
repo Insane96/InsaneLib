fbc68cbeed3a5a06fcdca43e8bd30c82feccf446
Insane96 <delvillano.alberto@gmail.com>
2026-02-17 15:44:13 +0100

    Ported IntegratedPack, needs testing, will port TimeStopNoPlayerOnline

---
+++ src/main/java/insane96mcp/insanelib/util/IntegratedPack.java
@@ -0,0 +1,130 @@
+package insane96mcp.insanelib.util;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.server.packs.PackLocationInfo;
+import net.minecraft.server.packs.PackSelectionConfig;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.PathPackResources;
+import net.minecraft.server.packs.repository.BuiltInPackSource;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
+import net.neoforged.fml.ModList;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.BooleanSupplier;
+
+public class IntegratedPack implements Comparable<IntegratedPack> {
+
+    public static final List<IntegratedPack> INTEGRATED_PACKS = new ArrayList<>();
+
+    int priority;
+    PackType packType;
+    String modId;
+    String path;
+    MutableComponent description;
+    BooleanSupplier enabled;
+
+    public IntegratedPack(int priority, PackType packType, String modId, String path, MutableComponent description, BooleanSupplier enabled) {
+        this.priority = priority;
+        this.packType = packType;
+        this.modId = modId;
+        this.path = path;
+        this.description = description;
+        this.enabled = enabled;
+    }
+
+    public IntegratedPack(PackType packType, String modId, String path, MutableComponent description, BooleanSupplier enabled) {
+        this(0, packType, modId, path, description, enabled);
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
+
+    public PackType getPackType() {
+        return this.packType;
+    }
+
+    public String getPath() {
+        return this.path;
+    }
+
+    public MutableComponent getDescription() {
+        return this.description;
+    }
+
+    public boolean shouldBeEnabled() {
+        return this.enabled.getAsBoolean();
+    }
+
+    @Override
+    public int compareTo(@NotNull IntegratedPack o) {
+        return Integer.compare(this.priority, o.priority);
+    }
+
+    @Override
+    public String toString() {
+        return "[%d] %s".formatted(this.priority, this.description.getString());
+    }
+
+    public static void addPack(IntegratedPack integratedPack) {
+        int index = Collections.binarySearch(INTEGRATED_PACKS, integratedPack, Comparator.comparingInt(IntegratedPack::getPriority));
+        if (index < 0)
+            index = -(index + 1);
+        INTEGRATED_PACKS.add(index, integratedPack);
+    }
+
+    public static void addServerPack(String modId, String path, String description, BooleanSupplier enabled) {
+        addServerPack(0, modId, path, description, enabled);
+    }
+
+    public static void addServerPack(int priority, String modId, String path, String description, BooleanSupplier enabled) {
+        addPack(new IntegratedPack(priority, PackType.SERVER_DATA, modId, path, Component.literal(description), enabled));
+    }
+
+    public static void addClientPack(String modId, String path, String description, BooleanSupplier enabled) {
+        addClientPack(0, modId, path, description, enabled);
+    }
+
+    public static void addClientPack(int priority, String modId, String path, String description, BooleanSupplier enabled) {
+        addPack(new IntegratedPack(priority, PackType.CLIENT_RESOURCES, modId, path, Component.literal(description), enabled));
+    }
+
+    public static void onAddPackFinders(AddPackFindersEvent event) {
+        for (var integratedPack : INTEGRATED_PACKS) {
+            if (event.getPackType() != integratedPack.getPackType()
+                    || !integratedPack.shouldBeEnabled())
+                continue;
+
+            Path resourcePath = ModList.get().getModFileById(integratedPack.modId).getFile().findResource("integrated_packs/" + integratedPack.getPath());
+            String packId = integratedPack.modId + ":" + integratedPack.getPath();
+
+            var locationInfo = new PackLocationInfo(
+                    packId,
+                    integratedPack.getDescription(),
+                    PackSource.DEFAULT,
+                    Optional.empty()
+            );
+
+            var selectionConfig = new PackSelectionConfig(
+                    integratedPack.shouldBeEnabled() && integratedPack.getPackType() != PackType.CLIENT_RESOURCES,
+                    Pack.Position.TOP,
+                    false
+            );
+
+            var pack = Pack.readMetaAndCreate(
+                    locationInfo,
+                    BuiltInPackSource.fromName(path -> new PathPackResources(path, resourcePath)),
+                    integratedPack.packType,
+                    selectionConfig
+            );
+
+            if (pack != null)
+                event.addRepositorySource(packConsumer -> packConsumer.accept(pack));
+        }
+    }
+}
