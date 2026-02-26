b3843e5892d0bd495ddac9dad91d7b4ecc262ae4
Insane96 <delvillano.alberto@gmail.com>
2026-02-26 13:25:12 +0100

    Added config option to limit invincibility frames to only faster attack speeds

--- src/main/java/insane96mcp/insanelib/module/base/AttackSpeedBasedInvincibility.java
+++ src/main/java/insane96mcp/insanelib/module/base/AttackSpeedBasedInvincibility.java
@@ -2,6 +2,7 @@ package insane96mcp.insanelib.module.base;
 
 import insane96mcp.insanelib.core.feature.Feature;
 import insane96mcp.insanelib.core.feature.LoadFeature;
+import insane96mcp.insanelib.core.feature.config.Config;
 import insane96mcp.insanelib.network.message.InvulnerableTimeSyncMessage;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -12,12 +13,17 @@ import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
 @LoadFeature(description = "Invincibility frames based off attack speed, so faster attacks will give less invincibility frames and viceversa.")
 public class AttackSpeedBasedInvincibility extends Feature {
+
+	@Config(description = "If true, invincibility frames will not be increased for slower attacks.")
+	public static Boolean onlyFasterAttackSpeed = false;
+
 	@SubscribeEvent
 	public void onAttack(LivingIncomingDamageEvent event) {
 		if (!this.isEnabled()
 				|| !(event.getSource().getEntity() instanceof LivingEntity livingEntity)
 				|| event.getEntity().invulnerableTime > 10
 				|| livingEntity.getAttribute(Attributes.ATTACK_SPEED) == null
+				|| (onlyFasterAttackSpeed && livingEntity.getAttribute(Attributes.ATTACK_SPEED).getValue() < 2f)
 				|| livingEntity.getMainHandItem().getAttributeModifiers().modifiers().stream().noneMatch(e -> e.attribute().is(Attributes.ATTACK_SPEED) && e.slot().test(EquipmentSlot.MAINHAND)))
 			return;
 
