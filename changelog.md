e0ef7b8dff95e9ecfb32601f0a3002c240a9b05e
Insane96 <delvillano.alberto@gmail.com>
2026-04-18 08:40:44 +0200

    2.4.12.0-beta - Added `SerializableMobEffectInstance`

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+## 2.4.12.0-beta
+### Technical
+* Added `SerializableMobEffectInstance`
+
 ## 2.4.11.0-beta
 * Added `insanelib:enchantability` item component
 
---
+++ src/main/java/insane96mcp/insanelib/data/SerializableMobEffectInstance.java
@@ -0,0 +1,117 @@
+package insane96mcp.insanelib.data;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.*;
+import com.google.gson.annotations.JsonAdapter;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+
+import java.util.ArrayList;
+
+/**
+ * Serializable version of MobEffectInstance
+ */
+@JsonAdapter(SerializableMobEffectInstance.Serializer.class)
+public class SerializableMobEffectInstance {
+	public final Holder<MobEffect> effect;
+	public int duration;
+	public int amplifier = 0;
+	public boolean ambient = false;
+	public boolean visible = true;
+	public boolean showIcon = true;
+
+	public SerializableMobEffectInstance(Holder<MobEffect> effect, int duration) {
+		this.effect = effect;
+		this.duration = duration;
+	}
+
+	public static class Builder {
+		public SerializableMobEffectInstance mobEffectInstance;
+
+		public Builder(Holder<MobEffect> effect, int duration) {
+			this.mobEffectInstance = new SerializableMobEffectInstance(effect, duration);
+		}
+
+		public Builder setAmplifier(int amplifier) {
+			this.mobEffectInstance.amplifier = amplifier;
+			return this;
+		}
+
+		public Builder ambientParticles() {
+			this.mobEffectInstance.ambient = true;
+			return this;
+		}
+
+		public Builder noParticles() {
+			this.mobEffectInstance.visible = false;
+			return this;
+		}
+
+		public Builder hideIcon() {
+			this.mobEffectInstance.showIcon = false;
+			return this;
+		}
+
+		public SerializableMobEffectInstance build() {
+			return this.mobEffectInstance;
+		}
+	}
+
+	public MobEffectInstance getMobEffectInstance() {
+		return new MobEffectInstance(this.effect, this.duration, this.amplifier, this.ambient, this.visible, this.showIcon);
+	}
+
+	public static final java.lang.reflect.Type LIST_TYPE = new TypeToken<ArrayList<SerializableMobEffectInstance>>(){}.getType();
+	public static class Serializer implements JsonDeserializer<SerializableMobEffectInstance>, JsonSerializer<SerializableMobEffectInstance> {
+		@Override
+		public SerializableMobEffectInstance deserialize(JsonElement json, java.lang.reflect.Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+			JsonObject jObject = json.getAsJsonObject();
+			String sId = GsonHelper.getAsString(jObject, "id", "");
+			ResourceLocation id = ResourceLocation.tryParse(sId);
+
+			if (id == null) {
+				throw new JsonParseException("Invalid id: %s".formatted(sId));
+			}
+			MobEffect mobEffect = BuiltInRegistries.MOB_EFFECT.get(id);
+			if (mobEffect == null) {
+				throw new JsonParseException("%s is not a known mob_effect".formatted(id));
+			}
+			int duration = GsonHelper.getAsInt(jObject, "duration");
+			Builder builder = new Builder(Holder.direct(mobEffect), duration);
+			if (jObject.has("amplifier"))
+				builder.setAmplifier(GsonHelper.getAsInt(jObject, "amplifier"));
+			if (jObject.has("amplifier"))
+				builder.setAmplifier(GsonHelper.getAsInt(jObject, "amplifier"));
+			if (GsonHelper.getAsBoolean(jObject, "ambient", false))
+				builder.ambientParticles();
+			if (GsonHelper.getAsBoolean(jObject, "hide_particles", false))
+				builder.noParticles();
+			if (GsonHelper.getAsBoolean(jObject, "hide_icon", false))
+				builder.hideIcon();
+
+			return builder.build();
+		}
+
+		@Override
+		public JsonElement serialize(SerializableMobEffectInstance src, java.lang.reflect.Type typeOfSrc, JsonSerializationContext context) {
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("id", BuiltInRegistries.MOB_EFFECT.getKey(src.effect.value()).toString());
+			jsonObject.addProperty("duration", src.duration);
+			if (src.amplifier > 0)
+				jsonObject.addProperty("amplifier", src.amplifier);
+			if (src.ambient)
+				jsonObject.addProperty("ambient", true);
+			if (!src.visible)
+				jsonObject.addProperty("hide_particles", true);
+			if (!src.showIcon)
+				jsonObject.addProperty("hide_icon", true);
+
+
+			return jsonObject;
+		}
+	}
+}
